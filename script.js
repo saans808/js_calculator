070dfd30f334308f16828b94831bc41e78e99a10
Saans <92422813+saans808@users.noreply.github.com>
2022-06-14 15:24:25 +0900

    ., +/-, % 구현 못함
    2차 삽질

--- script.js
+++ script.js
@@ -1,51 +1,89 @@
-
-
-
-
-
-class Calculator {
-    constructor(displayElement) {
-        this.displayElement = displayElement;
-        this.displayContent ='';
+let numOne = '';
+let operator ='';
+let numTwo = '';
+const $operator = document.querySelector('#operator');
+const $output = document.querySelector('#output'); 
+const onClickNumber = (event) => {
+    if (!operator) {
+        numOne += event.target.textContent;
+        $output.value += event.target.textContent;
+        return;
     }
-    appendNum(number) {
-        this.displayContent += Number;
+
+    if (!numTwo) {
+        $output.value = '';
     }
-    
-    appendResult(result) {
-        this.displayContent += result;
-    }
-    updateDisplay() {
-        this.displayElement.value = this.displayContent;
+    numTwo += event.target.textContent;
+    $output.value += event.target.textContent;
+};
+
+document.querySelector('#b0').addEventListener('click', onClickNumber);
+document.querySelector('#b1').addEventListener('click', onClickNumber);
+document.querySelector('#b2').addEventListener('click', onClickNumber);
+document.querySelector('#b3').addEventListener('click', onClickNumber);
+document.querySelector('#b4').addEventListener('click', onClickNumber);
+document.querySelector('#b5').addEventListener('click', onClickNumber);
+document.querySelector('#b6').addEventListener('click', onClickNumber);
+document.querySelector('#b7').addEventListener('click', onClickNumber);
+document.querySelector('#b8').addEventListener('click', onClickNumber);
+document.querySelector('#b9').addEventListener('click', onClickNumber);
+
+
+const onClickOperator = (op) => () => {
+    if (numOne) {
+        operator = op;
+        $operator.value = op;
+    } else {
+        alert('숫자를 입력하세요.');
     }
 }
 
 
-const buttons = calculator.querySelectorAll('button');
-const displayElement = document.querySelector('input');
-const calculator = new Calculator(displayElement);
-
-
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        switch (button.dataset.type) {
-            case 'operator':
-                console.log('operator');
-                break
-            case 'positiveNegative':
-                console.log('positiveNegative');
-                break
-            case 'percentage':
-                console.log('percentage');
-                break 
-            case 'AC':
-                console.log('AC');
-            case 'result':
-                console.log('result');
+document.querySelector('#divide').addEventListener('click', onClickOperator('÷'));
+document.querySelector('#multiply').addEventListener('click', onClickOperator('*'));
+document.querySelector('#minus').addEventListener('click', onClickOperator('-'));
+document.querySelector('#plus').addEventListener('click', onClickOperator('+'));
+document.querySelector('#calculate').addEventListener('click', () => {
+    if (numTwo) {
+        switch (operator) {
+            case '÷':
+                $output.value = numOne / numTwo;
+                break;
+            case '*':
+                $output.value = numOne * numTwo;
+                break;
+            case '-':
+                $output.value = numOne - numTwo;
+                break;
+            case '+':
+                $output.value = parseInt(numOne) + parseInt(numTwo);    
             default:
-                console.log('number');
-                break
+                break;
+            }
+            $operator.value = '';
+            numOne = $output.value;
+            operator = '';    
+            numTwo = '';   
+        } else {
+            alert('숫자를 입력하세요.');
         }
-    })
-})
+    });
+    document.querySelector('#AC').addEventListener('click', () => {
+        numOne = '';
+        operator = '';
+        numTwo = '';
+        $operator.value = '';
+        $output.value = '';
+    });
+ 
+
+document.querySelector('#dot').addEventListener('click', onClickNumber);   
+
+ document.querySelector('#positiveNegative').addEventListener('click', onClickNumber);
+
+ document.querySelector('#percentage').addEventListener('click', () => {
+    $output.value = $output.value % 100;    // 안됨
+ });
+
+ // . , +/-, % 구현 못함
+ // 일단 커밋해놓고 eval 써보고 다른 방법 또 찾기
